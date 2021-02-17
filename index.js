bd77ce83cad233cdff553403bb615b82b6aa8438
Taimoor Khan <46717905+Taimoorkhan1122@users.noreply.github.com>
2021-02-17 15:01:01 +0500

    FEAT: added colored console messages

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // importing modules
 const express = require("express");
 const { ConnectionBase } = require("mongoose");
+const chalk = require("chalk");
 require("dotenv").config({ path: "./.env" });
 
 // importing internal modules
@@ -16,37 +17,21 @@ connectDatabase();
 // when  we get data, it will be parsed into json rather than ugly raw data
 app.use(express.json({ extended: false }));
 
-// logger middleware
-// const logger = (req, res, next) => {
-//   if (req.method !== "GET") {
-//     console.log("Not found!");
-//     res.status(404).json({ message: "Request not found!" });
-//   }
-//   next();
-// };
-// app.use(logger);
-
 // ========= Application Routes =========
 app.get("/", (req, res) => {
   res.status(200).send("hello from express server");
 });
 
-// Auth middleware
-// function Auth(req, res, next) {
-//   console.log("Auth middleware triggered");
-//   res.name ? next() : res.status(400).send("You must login first!");
-// }
-
 // handling applications routes
 app.use("/api/v1", router);
 
-// We can use middleware to handle unmatched routes
+// This middleware will handle unmatched routes
 app.use((req, res) => {
   console.log(
     "Got request Method: ",
-    req.method,
-    " For: " + req.url,
-    " \nResponding with 400 NOT FOUND"
+    chalk.red.bold(req.method),
+    " For: " + chalk.green.bold(req.url),
+    " \n" + chalk.blue.bold("Responding with 400 NOT FOUND")
   );
   res.status(400).send("Request not found!");
 });
